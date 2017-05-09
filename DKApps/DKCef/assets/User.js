acd47058a1e437e32eca02fe663bbacaaa6f90ce
dkuser <dkuser@digitalknob.com>
2017-05-09 14:53:58 -0700

    commit from git

--- DKApps/DKCef/assets/User.js
+++ DKApps/DKCef/assets/User.js
@@ -6,23 +6,4 @@ var height = 600;
 DKCreate("DKCef,Cef,0,0,"+width+","+height+","+url);
 DK_SetFramerate(5);
 
-//var currentBrowser = DKCef_GetCurrentBrowser("Cef");
-//DKCef_SetUrl("Cef", url, currentBrowser);
-//DKCef_SetFocus("Cef");
-
 DKCreate("DKDebug/DKDebug.js");
-//DKAddEvent("GLOBAL", "resize", User_OnEvent);
-
-////////////////////////////
-function User_OnEvent(event)
-{	
-	//TODO
-	/*
-	if(DK_Type(event, "resize")){
-		//DKLog("User_OnEvent(): resize\n", DKINFO);
-		var width = DKWindow_GetWidth();
-		var height = DKWindow_GetHeight();
-		DK_CallFunc("Cef::OnResize", "0,0,"+String(width)+","+String(height));
-	}
-	*/
-}
