471ce16c36d6d4dbfd6e10ca7866de2f85bedbf1
dkuser <dkuser@digitalknob.com>
2017-05-09 13:53:07 -0700

    commit from git

--- DKApps/DKCef/assets/User.js
+++ DKApps/DKCef/assets/User.js
@@ -1,6 +1,6 @@
 var url = "google.com";
 //var url = "http://digitalknob.com";
-//var url = "chrome://gpu"; 
+//var url = "chrome://gpu";
 var width = 800;
 var height = 600;
 DKCreate("DKCef,Cef,0,0,"+width+","+height+","+url);
