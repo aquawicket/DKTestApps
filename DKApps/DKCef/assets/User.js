6615bb209a8bf67d33be8fba8b9dbcea7bb23a87
dkuser <dkuser@digitalknob.com>
2017-05-09 20:32:20 -0700

    commit from git

--- DKApps/DKCef/assets/User.js
+++ DKApps/DKCef/assets/User.js
@@ -8,6 +8,7 @@ DKCreate("DKCef,Cef,0,0,"+width+","+height+","+url);
 DK_SetFramerate(5);
 DKCreate("DKDebug/DKDebug.js");
 
+
 DKAddEvent("GLOBAL", "DKCef_OnLoadingStateChange", User_OnEvent);
 ////////////////////////////
 function User_OnEvent(event)  //Duktape
@@ -15,8 +16,9 @@ function User_OnEvent(event)  //Duktape
 	DKLog("User_OnEvent("+DK_GetId(event)+","+DK_GetType(event)+","+DK_GetValue(event)+")\n", DKINFO);
 
 	//Alter the google page logo
-	DKCef_RunJavascript("Cef", "document.getElementById('hplogo').setAttribute('src','http://digitalknob.com/DKSearch/DKSearch/logo.png');");
-
-	//Create Url Bar
-	//We need to do this in a overlayed browser somehow
+	if(DK_Type(event, "DKCef_OnLoadingStateChange")){
+		if(DKCef_GetUrl("Cef", 0) == "http://www.google.com/"){
+			DKCef_RunJavascript("Cef", "document.getElementById('hplogo').setAttribute('src','http://digitalknob.com/DKSearch/DKSearch/logo.png');");
+		}
+	}
 }
