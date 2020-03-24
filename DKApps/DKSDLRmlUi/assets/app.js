9225f29ad290501421763d48fa57a3a01e272666
dkuser <dkuser@digitalknob.com>
2020-03-24 14:52:24 -0700

    commit from git

--- DKApps/DKSDLRmlUi/assets/app.js
+++ DKApps/DKSDLRmlUi/assets/app.js
@@ -1,3 +1,13 @@
 DKCreate("DKWindow");
 DKCreate("DKRml");
-DKRml_LoadUrl("C:/digitalknob/DKTestApps/DKApps/DKSDLRmlUi/assets/test.html");
+//DKRml_ToggleDebugger();
+
+location.href = "C:/digitalknob/DKTestApps/DKApps/DKSDLRmlUi/assets/test.html";
+
+console.log("window = "+window);
+console.log("location = "+location);
+console.log("document = "+document);
+
+console.log(document.getElementById("test.html").innerHTML);
+document.getElementById("test.html").innerHTML = "<img id=\"gif_image\" src=\"test.gif\" style=\"position:absolute;left:10%;top:35%;width:40%;\"></img>"
+console.log(document.getElementById("test.html").innerHTML);
