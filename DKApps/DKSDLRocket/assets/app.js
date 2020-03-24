3ccdb112b4c5d6b6e10aa0b7d0b0a9ba174619d9
dkuser <dkuser@digitalknob.com>
2020-03-23 21:50:30 -0700

    commit from git

--- DKApps/DKSDLRocket/assets/app.js
+++ DKApps/DKSDLRocket/assets/app.js
@@ -1,7 +1,7 @@
 DKCreate("DKWindow");
 DKCreate("DKRocket");
 //DKCreate("DKWidget");
-DKCreate("index.html");
+DKCreate("test.html");
 //DKCreate("DKGui/DKFrame.js");
 //DKCreate("DKDebug/DKDebug.js");
 //DKCreate("scale.js");
