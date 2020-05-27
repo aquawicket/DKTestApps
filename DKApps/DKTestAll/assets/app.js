4a9701fafb8d28b97f0fef8900eecf874b5086f6
dkuser <dkuser@digitalknob.com>
2020-05-27 15:24:36 -0500

    commit from git

--- DKApps/DKTestAll/assets/app.js
+++ DKApps/DKTestAll/assets/app.js
@@ -1,6 +1,16 @@
+DKCreate("DKDebug/DKDebug.js");
+
 var url = DKAssets_LocalAssets()+"DKWebTest/index.html";
 
-//// Create SDL Rocket window
+/*
+//// Create Cef window ////
+var USE_CEF = 1;
+DKCreate("DKCef,Cef,0,0,800,600,"+url);
+DKCef_NewBrowser("Cef",5,5,screen.width/2-10,screen.height-45,url);
+//DKCef_ShowDevTools(0);
+*/
+
+//// Create SDL Rocket window ////
 DKCreate("DKWindow");
 DKCreate("DKRocket");
 location.href = url;
@@ -8,29 +18,12 @@ window.moveTo(screen.width/2, 36);
 window.resizeTo(screen.width / 2 - 10, screen.height-84);
 //DKRocket_ToggleDebugger();
 
-
-
-////////////////// OLD CODE ///////////////////
 /*
-var USE_CEF = 0;
-
-DKCreate("DKDebug/DKDebug.js");
-
-var welcome = "\n";
-welcome = "######################### \n";
-welcome += "####### DKTestAll ####### \n";
-welcome += "######################### \n";
-console.log(welcome);
-
-
-//TestJavascript
-DKCreate("TestJavascript.js");
-DKClose("TestJavascript.js");
-
-//TestWindow
-DKCreate("TestWindow.js");
-
-//TestRocket
-DKCreate("TestRocket.js");
+//// Create SDL Rml window ////
+DKCreate("DKWindow");
+DKCreate("DKRml");
+location.href = url;
+window.moveTo(screen.width/2, 36);
+window.resizeTo(screen.width / 2 - 10, screen.height-84);
+//DKRml_ToggleDebugger();
 */
-//////////////////////////////////////////////
