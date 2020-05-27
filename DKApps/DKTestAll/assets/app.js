ee3cf2b8b4d6d7d84ce45185e78a00f6a15ba4e0
dkuser <dkuser@digitalknob.com>
2020-05-27 16:34:38 -0500

    commit from git

--- DKApps/DKTestAll/assets/app.js
+++ DKApps/DKTestAll/assets/app.js
@@ -2,18 +2,16 @@ DKCreate("DKDebug/DKDebug.js");
 
 var url = DKAssets_LocalAssets()+"DKWebTest/index.html";
 
-/*
 //// Create Cef window ////
 var USE_CEF = 1;
 DKCreate("DKCef,Cef,0,0,800,600,"+url);
 DKCef_NewBrowser("Cef",5,5,screen.width/2-10,screen.height-45,url);
-//DKCef_ShowDevTools(0);
-*/
+DKCef_ShowDevTools(0);
 
 //// Create SDL Rocket window ////
 DKCreate("DKWindow");
-DKCreate("DKRocket");
-location.href = url;
+DKCreate("DKRml");
+//location.href = url;
 window.moveTo(screen.width/2, 36);
 window.resizeTo(screen.width / 2 - 10, screen.height-84);
 //DKRocket_ToggleDebugger();
@@ -27,3 +25,9 @@ window.moveTo(screen.width/2, 36);
 window.resizeTo(screen.width / 2 - 10, screen.height-84);
 //DKRml_ToggleDebugger();
 */
+
+//// Create Widget ////
+DKCreate("DK/DK.js");
+DKCreate("DK/DK.css");
+DKCreate("DKWidget/DKWidget.js");
+DKCreate("DKColorPicker/DKColorPicker.js");
