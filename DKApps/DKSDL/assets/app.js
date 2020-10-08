0fabecb38021d56c82315a89d3d9445a9265d493
dkuser <dkuser@digitalknob.com>
2020-10-07 21:52:59 -0700

    commit from git

--- DKApps/DKSDL/assets/app.js
+++ DKApps/DKSDL/assets/app.js
@@ -1,5 +1,5 @@
-DKCreate("DKSDLWindow");
-DKCreate("DKWindow");
+CPP_DKDuktape_Create("DKSDLWindow");
+CPP_DKDuktape_Create("DKWindow");
 
 console.log("DKWindow_TestInt(1234) = "+DKWindow_TestInt(1234)+"\n");
 console.log("DKWindow_TestString(\"test string\") = "+DKWindow_TestString("test string")+"\n");
@@ -8,13 +8,13 @@ console.log("DKWindow_TestReturnString() = "+DKWindow_TestReturnString()+"\n");
 
 /////////////////////////
 //ANDROID back key = exit
-DKAddEvent("window", "keydown", User_OnEvent);
-function User_OnEvent(event)  //Duktape
-{
-	if(DK_Type(event, "keydown")){
+document.addEventListener("keydown", User_OnEvent);
+
+function User_OnEvent(event){
+	if(event.type === "keydown"){
 		console.log("KEYDOWN: "+DK_GetValue(event));
-		if(DK_GetValue(event) == "4"){ //Exit for ANDROID
-		    DK_Exit();
+		if(event.value == "4"){ //FIXME: Exit for ANDROID
+		    CPP_DKDuktape_Exit();
 		}
 	}
 }
