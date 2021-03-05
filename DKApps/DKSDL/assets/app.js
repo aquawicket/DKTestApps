91a19067cbda077ca0f790375a71099a2b22683f
dkuser <dkuser@digitalknob.com>
2021-03-04 23:43:12 -0800

    commit from git

--- DKApps/DKSDL/assets/app.js
+++ DKApps/DKSDL/assets/app.js
@@ -1,19 +1,19 @@
 CPP_DKDuktape_Create("DKSDLWindow");
 CPP_DKDuktape_Create("DKWindow");
 
-console.log("DKWindow_TestInt(1234) = "+DKWindow_TestInt(1234)+"\n");
-console.log("DKWindow_TestString(\"test string\") = "+DKWindow_TestString("test string")+"\n");
-console.log("DKWindow_TestReturnInt() = "+DKWindow_TestReturnInt()+"\n");
-console.log("DKWindow_TestReturnString() = "+DKWindow_TestReturnString()+"\n");
+console.log("CPP_DKWindow_TestInt(1234) = "+CPP_DKWindow_TestInt(1234)+"\n");
+console.log("CPP_DKWindow_TestString(\"test string\") = "+CPP_DKWindow_TestString("test string")+"\n");
+console.log("CPP_DKWindow_TestReturnInt() = "+CPP_DKWindow_TestReturnInt()+"\n");
+console.log("CPP_DKWindow_TestReturnString() = "+CPP_DKWindow_TestReturnString()+"\n");
 
 /////////////////////////
 //ANDROID back key = exit
-document.addEventListener("keydown", User_OnEvent);
+window.addEventListener("keydown", User_OnEvent);
 
 function User_OnEvent(event){
 	if(event.type === "keydown"){
-		console.log("KEYDOWN: "+DK_GetValue(event));
-		if(event.value == "4"){ //FIXME: Exit for ANDROID
+		console.log("KEYDOWN: "+event.value);
+		if(event.value == "4"){ //FIXME: Exit for ANDROID button
 		    CPP_DKDuktape_Exit();
 		}
 	}
