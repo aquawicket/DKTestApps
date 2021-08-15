b47f1e3077924fff0272710011414402c815563c
aquawicket <aquawicket@hotmail.com>
2021-08-14 23:58:09 -0700

    commit from git

--- DKApps/DKCore/Build.sh
+++ DKApps/DKCore/Build.sh
@@ -10,10 +10,10 @@ find . -name "CMakeCache.*" -delete
 #find . -type d -name "CMakeFiles" -exec rm -rf {} \;
 rm -rf `find . -type d -name CMakeFiles`
 		
-mkdir /home/"$USER"/digitalknob/DK/DKApps/$APP/$OS
-mkdir /home/"$USER"/digitalknob/DK/DKApps/$APP/$OS/$TYPE
-cd /home/"$USER"/digitalknob/DK/DKApps/$APP/$OS/$TYPE
-rm /home/"$USER"/digitalknob/DK/DKApps/$APP/$OS/$TYPE/CMakeCache.txt
+mkdir /home/"$USER"/digitalknob/DKTestApps/DKApps/$APP/$OS
+mkdir /home/"$USER"/digitalknob/DKTestApps/DKApps/$APP/$OS/$TYPE
+cd /home/"$USER"/digitalknob/DKTestApps/DKApps/$APP/$OS/$TYPE
+rm /home/"$USER"/digitalknob/DKTestApps/DKApps/$APP/$OS/$TYPE/CMakeCache.txt
 if [[ $TYPE -eq "Debug" ]]
 then
 	echo cmake -G "Unix Makefiles" -DDEBUG=ON -DREBUILDALL=ON -DSTATIC=ON /home/"$USER"/digitalknob/DK
@@ -24,8 +24,8 @@ then
 	echo cmake -G "Unix Makefiles" -DRELEASE=ON -DREBUILDALL=ON -DSTATIC=ON /home/"$USER"/digitalknob/DK
 	cmake -G "Unix Makefiles" -DRELEASE=ON -DREBUILDALL=ON -DSTATIC=ON /home/"$USER"/digitalknob/DK
 fi
-#chmod +x /home/"$USER"/digitalknob/DK/DKBuilder.sh
+#chmod +x /home/"$USER"/digitalknob/DKTestApps/DKBuilder.sh
 
-#cd /home/"$USER"/digitalknob/DK/DKApps/$APP/$OS/$TYPE
+#cd /home/"$USER"/digitalknob/DKTestApps/DKApps/$APP/$OS/$TYPE
 make $APP
-#chmod +x /home/"$USER"/digitalknob/DK/DKApps/$APP/$OS/$TYPE/$APP
+#chmod +x /home/"$USER"/digitalknob/DKTestApps/DKApps/$APP/$OS/$TYPE/$APP
