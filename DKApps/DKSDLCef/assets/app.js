b7109ee0784fa09f99aadba1c9ea22bafc093768
aquawicket <aquawicket@hotmail.com>
2021-09-14 17:15:51 -0700

    commit from git

--- DKApps/DKSDLCef/assets/app.js
+++ DKApps/DKSDLCef/assets/app.js
@@ -1,12 +1,13 @@
-//var USE_CEF = 1;
-CPP_DK_Create("DKWindow");
+var USE_CEF = 1;
+//CPP_DK_Create("DKWindow");
+CPP_DK_Create("DKSDLWindow");
 //var url = "file:///C:/Users/aquawicket/digitalknob/DKApps/DKIDE/assets/index.html";
 var url = "http://google.com";
 var width = 800;//DKWindow_GetWidth();
 var height = 600;//DKWindow_GetHeight();
-DKCreate("DKCef,CefSDL,0,0,"+width+","+height+","+url);
-DKCef_NewBrowser("CefSDL",0,0,width,height,url);
-var currentBrowser = DKCef_GetCurrentBrowser("CefSDL");
+CPP_DK_Create("DKCef,CefSDL,0,0,"+width+","+height+","+url);
+CPP_DKCef_NewBrowser("CefSDL",0,0,width,height,url);
+//var currentBrowser = DKCef_GetCurrentBrowser("CefSDL");
 DKCef_SetUrl(0, url, currentBrowser);
 //DKCef_SetFocus("CefSDL");
 
