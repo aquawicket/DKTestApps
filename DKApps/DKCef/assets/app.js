009bbf33cc2df6a6c31710997b76c0b87827f780
aquawicket <aquawicket@hotmail.com>
2021-09-28 16:09:29 -0700

    commit from git

--- DKApps/DKCef/assets/app.js
+++ DKApps/DKCef/assets/app.js
@@ -1,9 +1,8 @@
 var USE_CEF = 1;
-var url = "http://www.google.com/";
-//var url = "chrome://gpu";
-//var url = "http://127.0.0.1:2393"
+//var url = "http://www.duckduckgo.com/";
+var url = "chrome://gpu";
 
-var width = 800;
-var height = 600;
-CPP_DK_Create("DKCef,Cef,0,0,"+width+","+height+","+url);
+var width = 1240;
+var height = 900;
+CPP_DK_Create("DKCef");
 CPP_DKCef_NewBrowser("Cef",0,0,width,height,url);
