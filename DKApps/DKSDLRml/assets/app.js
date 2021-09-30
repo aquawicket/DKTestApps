93a504aa17cc564a406e45783ba01502dc0562f9
aquawicket <aquawicket@hotmail.com>
2021-09-29 23:36:28 -0700

    commit from git

--- DKApps/DKSDLRml/assets/app.js
+++ DKApps/DKSDLRml/assets/app.js
@@ -2,9 +2,24 @@ CPP_DK_Create("DKWindow")
 CPP_DK_Create("DKRml")
 
 let ele = document.createElement("div")
+ele.id = "idvalue"
+ele.setAttribute("class", "classvalue")
 ele.setAttribute("test", "testvalue")
-ele.id = "element1"
+ele.style.display = "block"
 ele.style.width = "100px"
 ele.style.height = "100px"
-//ele.style.backgroundColor = "rgb(200,0,0)"
+//ele.style.backgroundColor = "black"
 document.body.appendChild(ele)
+
+
+
+ele.ele2 = document.createElement("div")
+ele.ele2.id = "idvalue2"
+ele.ele2.setAttribute("class", "classvalue2")
+ele.ele2.setAttribute("test2", "testvalue2")
+ele.ele2.style.position = "absolute"
+ele.ele2.style.display = "block"
+ele.ele2.style.width = "66px"
+ele.ele2.style.height = "66px"
+//ele.ele2.style.backgroundColor = "white";
+ele.appendChild(ele.ele2)
