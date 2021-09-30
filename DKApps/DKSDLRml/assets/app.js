a9614bd085a57d7488386752860b238508dac4e5
aquawicket <aquawicket@hotmail.com>
2021-09-30 04:51:40 -0700

    commit from git

--- DKApps/DKSDLRml/assets/app.js
+++ DKApps/DKSDLRml/assets/app.js
@@ -1,23 +1,38 @@
+const url = CPP_DKAssets_LocalAssets()+"index.html"
+//const url = CPP_DKAssets_LocalAssets()+"DKWebTest/index.html"
+
+
+//// Create SDLRml window
+const sdl_x = window.screen.width / 2
+const sdl_y = 0
+const sdl_width = window.screen.width / 2
+const sdl_height = window.screen.height
+const sdl_x_offset = 0
+const sdl_y_offest = 30
+const sdl_width_offset = 1
+const sdl_height_offset = -30
 CPP_DK_Create("DKWindow")
 CPP_DK_Create("DKRml")
+window.resizeTo(sdl_width+sdl_width_offset, sdl_height+sdl_height_offset)
+window.moveTo(sdl_x+sdl_x_offset, sdl_y+sdl_y_offest)
+location.href = url
+window.addEventListener("keydown", function(event){
+	CPP_DK_Create("DKWindow/DKWindow.js")
+	console.log(event.code)
+	if(event.code === "F12")
+		CPP_DKRml_DebuggerToggle()
+	if(event.code === "F11")
+		CPP_DKWindow_IsFullscreen() ? CPP_DKWindow_Windowed() : dk.window.fullscreen()
+})
 
-let ele = document.createElement("div")
-ele.id = "idvalue"
-ele.setAttribute("class", "classvalue")
-ele.setAttribute("test", "testvalue")
-ele.style.display = "block"
-ele.style.width = "100px"
-ele.style.height = "100px"
-//ele.style.backgroundColor = "black"
-document.body.appendChild(ele)
 
-let ele2 = document.createElement("div")
-ele2.id = "idvalue2"
-ele2.setAttribute("class", "classvalue2")
-ele2.setAttribute("test2", "testvalue2")
-//ele2.style.position = "absolute"
-ele2.style.display = "block"
-ele2.style.width = "66px"
-ele2.style.height = "66px"
-//ele.ele2.style.backgroundColor = "white";
-document.body.appendChild(ele2)
+//// Console window
+const console_x = 0
+const console_y = 0
+const console_width = window.screen.width / 2
+const console_height = window.screen.height
+const console_x_offset = -8
+const console_y_offest = -1
+const console_width_offset = +16
+const console_height_offset = +9
+CPP_DK_setConsolePosition(console_x+console_x_offset, console_y+console_y_offest, console_width+console_width_offset, console_height+console_height_offset)
--- DKApps/DKSDLRml/assets/myapp.js
+++ DKApps/DKSDLRml/assets/myapp.js
@@ -8,26 +8,41 @@ myapp.loadFiles = function myapp_loadFiles() {
 
 myapp.loadApp = function myapp_loadApp() {
 	console.log("myapp.loadApp()")
-	let ele = document.createElement("div")
-	ele.id = "idvalue"
-	ele.setAttribute("class", "classvalue")
-	ele.setAttribute("test", "testvalue")
-	//ele.style.display = "block"
-	ele.style.width = "100px"
-	ele.style.height = "100px"
-	//ele.style.backgroundColor = "black"
-	document.body.appendChild(ele)
+	let ele1 = document.createElement("div")
+	ele1.id = "idvalue1"
+	ele1.setAttribute("class", "classvalue1")
+	ele1.setAttribute("test1", "testvalue1")
+	//ele1.style.display = "block"
+	ele1.style.width = "100px"
+	ele1.style.height = "100px"
+	document.body.appendChild(ele1)
 
 	let ele2 = document.createElement("div")
 	ele2.id = "idvalue2"
 	ele2.setAttribute("class", "classvalue2")
 	ele2.setAttribute("test2", "testvalue2")
-	//ele2.style.position = "absolute"
 	//ele2.style.display = "block"
-	ele2.style.width = "66px"
-	ele2.style.height = "66px"
-	//ele.ele2.style.backgroundColor = "white";
+	ele2.style.width = "75px"
+	ele2.style.height = "75px"
 	document.body.appendChild(ele2)
+	
+	let ele3 = document.createElement("div")
+	ele3.id = "idvalue3"
+	ele3.setAttribute("class", "classvalue3")
+	ele3.setAttribute("test3", "testvalue3")
+	//ele3.style.display = "block"
+	ele3.style.width = "50px"
+	ele3.style.height = "50px"
+	document.body.appendChild(ele3)
+	
+	let ele4 = document.createElement("div")
+	ele4.id = "idvalue4"
+	ele4.setAttribute("class", "classvalue4")
+	ele4.setAttribute("test4", "testvalue4")
+	//ele4.style.display = "block"
+	ele4.style.width = "25px"
+	ele4.style.height = "25px"
+	document.body.appendChild(ele4)
 }
 
 DUKTAPE && myapp.loadFiles();
