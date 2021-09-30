6aba6c7736c381f79aa78e24e4ee3900fd3b5ea2
aquawicket <aquawicket@hotmail.com>
2021-09-30 12:59:42 -0700

    commit from git

--- DKApps/DKSDLRml/assets/myapp.js
+++ DKApps/DKSDLRml/assets/myapp.js
@@ -12,16 +12,14 @@ myapp.loadApp = function myapp_loadApp() {
 	ele1.id = "idvalue1"
 	ele1.setAttribute("class", "classvalue1")
 	ele1.setAttribute("test", "testvalue1")
-	//ele1.style.display = "block"
-	ele1.style.width = "100px"
-	ele1.style.height = "100px"
+	ele1.style.width = "75px"
+	ele1.style.height = "75px"
 	document.body.appendChild(ele1)
 
 	let ele2 = document.createElement("div")
 	ele2.id = "idvalue2"
 	ele2.setAttribute("class", "classvalue2")
 	ele2.setAttribute("test", "testvalue2")
-	//ele2.style.display = "block"
 	ele2.style.width = "75px"
 	ele2.style.height = "75px"
 	document.body.appendChild(ele2)
@@ -30,19 +28,41 @@ myapp.loadApp = function myapp_loadApp() {
 	ele3.id = "idvalue3"
 	ele3.setAttribute("class", "classvalue3")
 	ele3.setAttribute("test", "testvalue3")
-	//ele3.style.display = "block"
-	ele3.style.width = "50px"
-	ele3.style.height = "50px"
+	ele3.style.width = "75px"
+	ele3.style.height = "75px"
 	document.body.appendChild(ele3)
 	
 	let ele4 = document.createElement("div")
 	ele4.id = "idvalue4"
 	ele4.setAttribute("class", "classvalue4")
 	ele4.setAttribute("test", "testvalue4")
-	//ele4.style.display = "block"
-	ele4.style.width = "25px"
-	ele4.style.height = "25px"
+	ele4.style.width = "75px"
+	ele4.style.height = "75px"
 	document.body.appendChild(ele4)
+	
+	let ele5 = document.createElement("div")
+	ele5.id = "idvalue5"
+	ele5.setAttribute("class", "classvalue5")
+	ele5.setAttribute("test", "testvalue5")
+	ele5.style.width = "75px"
+	ele5.style.height = "75px"
+	document.body.appendChild(ele5)
+	
+	let ele6 = document.createElement("div")
+	ele6.id = "idvalue6"
+	ele6.setAttribute("class", "classvalue6")
+	ele6.setAttribute("test", "testvalue6")
+	ele6.style.width = "75px"
+	ele6.style.height = "75px"
+	document.body.appendChild(ele6)
+	
+	let ele7 = document.createElement("div")
+	ele7.id = "idvalue6"
+	ele7.setAttribute("class", "classvalue7")
+	ele7.setAttribute("test", "testvalue7")
+	ele7.style.width = "75px"
+	ele7.style.height = "75px"
+	document.body.appendChild(ele7)
 }
 
 DUKTAPE && myapp.loadFiles();
--- DKApps/SDLRml/DKMAKE.cmake
+++ DKApps/SDLRml/DKMAKE.cmake
@@ -7,7 +7,7 @@ DKDEPEND(tiff)
 DKDEPEND(glew)
 DKDEPEND(sdl2)
 DKDEPEND(sdl2_image)
-DKDEPEND(rmlui)
+DKDEPEND(rmlui rmlui_debugger)
 
 DKINCLUDE(${DKPROJECT})
 DKINCLUDE(${DKPROJECT}/shell/include)
