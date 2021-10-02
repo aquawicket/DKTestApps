219b9da5aa0f1a5e384343762f59d63587247250
aquawicket <aquawicket@hotmail.com>
2021-10-01 18:47:17 -0700

    commit from git

--- DKApps/SDLRml/sdl2/src/main.cpp
+++ DKApps/SDLRml/sdl2/src/main.cpp
@@ -116,7 +116,7 @@ int main(int /*argc*/, char** /*argv*/)
 
 	Rml::Debugger::Initialise(Context);
 
-	Rml::ElementDocument* Document = Context->LoadDocument("../../assets/index.rml");
+	Rml::ElementDocument* Document = Context->LoadDocument("../../assets/index.html");
 
 	if (Document)
 	{
