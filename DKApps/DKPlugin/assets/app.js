da702e361aaa67bd969b69f1f36933470ece9bac
aquawicket <aquawicket@hotmail.com>
2021-10-08 14:51:02 -0700

    commit from git

--- DKApps/DKPlugin/assets/app.js
+++ DKApps/DKPlugin/assets/app.js
@@ -1,5 +1,5 @@
 var datapath = DKAssets_GetDataPath();
-DKLog("datapath: "+datapath+"\n");
+console.log("datapath: "+datapath+"\n");
 DKLoadPlugin(datapath+"DKPlugin1/DKPlugin1.dll");
 DK_CallFunc("DKPlugin1::SomeFunction","");
 DKLoadPlugin(datapath+"DKPlugin2/DKPlugin2.dll");
