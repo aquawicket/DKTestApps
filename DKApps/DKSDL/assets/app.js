1874247be7d70bf696b3e9e5a40b090950842e30
aquawicket <aquawicket@hotmail.com>
2021-10-08 15:08:22 -0700

    commit from git

--- DKApps/DKSDL/assets/app.js
+++ DKApps/DKSDL/assets/app.js
@@ -1,21 +1,20 @@
-CPP_DK_Create("DKSDLWindow");
-CPP_DK_Create("DKWindow");
+CPP_DK_Create("DKSDLWindow")
+CPP_DK_Create("DKWindow")
 
-console.log("CPP_DKWindow_TestInt(1234) = "+CPP_DKWindow_TestInt(1234)+"\n");
-console.log("CPP_DKWindow_TestString(\"test string\") = "+CPP_DKWindow_TestString("test string")+"\n");
-console.log("CPP_DKWindow_TestReturnInt() = "+CPP_DKWindow_TestReturnInt()+"\n");
-console.log("CPP_DKWindow_TestReturnString() = "+CPP_DKWindow_TestReturnString()+"\n");
+console.log("CPP_DKWindow_TestInt(1234) = "+CPP_DKWindow_TestInt(1234)+"\n")
+console.log("CPP_DKWindow_TestString(\"test string\") = "+CPP_DKWindow_TestString("test string")+"\n")
+console.log("CPP_DKWindow_TestReturnInt() = "+CPP_DKWindow_TestReturnInt()+"\n")
+console.log("CPP_DKWindow_TestReturnString() = "+CPP_DKWindow_TestReturnString()+"\n")
 
-/////////////////////////
+
 //ANDROID back key = exit
-window.addEventListener("keydown", User_OnEvent);
+window.addEventListener("keydown", User_OnEvent)
 
 function User_OnEvent(event){
 	if(event.type === "keydown"){
-		console.log("KEYDOWN: "+event.value);
+		console.log("KEYDOWN: "+event.value)
 		if(event.value == "4"){ //FIXME: Exit for ANDROID button
-		    CPP_DK_Exit();
+		    CPP_DK_Exit()
 		}
 	}
 }
-//////////////////////////
