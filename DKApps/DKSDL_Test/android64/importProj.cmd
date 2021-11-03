0aefb607166382db76fb08bcd5a2e7aea7a180b1
aquawicket <aquawicket@hotmail.com>
2021-11-03 06:25:13 -0700

    commit from git

--- DKApps/DKSDL_Test/android64/importProj.cmd
+++ DKApps/DKSDL_Test/android64/importProj.cmd
@@ -1,2 +1,1 @@
-set AndroidTargetsPath="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Microsoft\MDD\Android\V150"
-dotnet sln "C:\Users\IK\digitalknob\DKTestApps\DKApps\DKSDL_Test\android64\DKSDL_Test.sln" add "C:\Users\IK\digitalknob\DKTestApps\DKApps\DKSDL_Test\android64\dkapp.Packaging.androidproj"
+dotnet sln "DKSDL_Test.sln" add "dkapp.Packaging.androidproj"
