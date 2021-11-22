737aaeb1576b35cb9be3998307d6f075cd317fd6
aquawicket <aquawicket@hotmail.com>
2021-11-22 08:49:18 -0800

    commit from git

--- DKApps/SDLRml/FindSamplesRoot.cpp
+++ DKApps/SDLRml/FindSamplesRoot.cpp
@@ -2,14 +2,16 @@
 #include "Shell.h"
 #ifdef RMLUI_PLATFORM_WIN32
 	#include "Shlwapi.h"
+#else
+    #include <sys/stat.h>
 #endif
+    
 
 Rml::String Shell::FindSamplesRoot() 
 {
 	Rml::String path = "";
 	
 #ifdef RMLUI_PLATFORM_WIN32
-#include "Shlwapi.h"
 	if(path.empty() && PathFileExistsA("../Samples"))
 		path = "../Samples";
 	if(path.empty() && PathFileExistsA("../../Samples"))
@@ -23,9 +25,27 @@ Rml::String Shell::FindSamplesRoot()
 	GetFullPathName(path.c_str(), 256, resolved_path, &fileExt);
 	Rml::String out(resolved_path);
 #else
-	char *resolved_path;
-	resolved_path = realpath(path.c_str(), NULL);
-	Rml::String out = resolved_path;
+    struct stat buffer;
+    if(path.empty() && (stat ("../Samples", &buffer) == 0) )
+        path = "../Samples";
+    if(path.empty() && (stat ("../../Samples", &buffer) == 0) )
+        path = "../../Samples";
+    if(path.empty() && (stat ("../../../Samples", &buffer) == 0) )
+        path = "../../../Samples";
+    if(path.empty() && (stat ("../../../../Samples", &buffer) == 0) )
+        path = "../../../../Samples";
+    if(path.empty() && (stat ("../../../../../Samples", &buffer) == 0) )
+        path = "../../../../../Samples";
+    if(path.empty() && (stat ("../../../../../../Samples", &buffer) == 0) )
+        path = "../../../../../../Samples";
+    if (path.empty() && (stat ("../assets", &buffer) == 0) )
+        path = "../assets";
+    if (path.empty() && (stat ("../../assets", &buffer) == 0) )
+        path = "../../assets";
+	char *resolved_path = realpath(path.c_str(), NULL);
+    if(!resolved_path)
+        return "";
+    Rml::String out(resolved_path);
 #endif
-	return out+"/";
+	return out;
 }
