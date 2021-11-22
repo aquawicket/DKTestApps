e414c6baf0839f9dc6656c2e3ed9c63798f38293
aquawicket <aquawicket@hotmail.com>
2021-11-22 11:42:46 -0800

    commit from git

---
+++ DKApps/SFMLRml/FindSamplesRoot.cpp
@@ -0,0 +1,89 @@
+#include <RmlUi/Core.h>
+#include "Shell.h"
+#include <filesystem>
+#ifdef RMLUI_PLATFORM_WIN32
+	#include "Shlwapi.h"
+#else
+    #include <sys/stat.h>
+    #include <unistd.h>
+	#include <mach-o/dyld.h>
+	#include <limits.h>
+#endif
+    
+
+Rml::String Shell::FindSamplesRoot() 
+{
+    Rml::String path = "";
+#ifdef WIN32
+    path = "";
+#else
+    path = "";
+    
+#endif
+#ifdef RMLUI_PLATFORM_WIN32
+	if(path.empty() && PathFileExistsA("../Samples"))
+		path = "../Samples";
+	if(path.empty() && PathFileExistsA("../../Samples"))
+		path = "../../Samples";
+	if(path.empty() && PathFileExistsA("../../../Samples"))
+		path = "../../../Samples";
+	if (path.empty() && PathFileExistsA("../assets"))
+		path = "../assets";
+	if (path.empty() && PathFileExistsA("../../assets"))
+		path = "../../assets";
+	if (path.empty() && PathFileExistsA("../../../assets"))
+		path = "../../../assets";
+	char *fileExt;
+	char resolved_path[256];
+	GetFullPathName(path.c_str(), 256, resolved_path, &fileExt);
+	Rml::String out(resolved_path);
+#else
+	char *resolved_path = "";
+	char buf [PATH_MAX];
+	uint32_t bufsize = PATH_MAX;
+	if(!_NSGetExecutablePath(buf, &bufsize))
+    puts(buf);
+	Rml::String appPath(buf);
+	
+	testPath = appPath+"/Samples";
+	if(!resolved_path) resolved_path = realpath(testPath.c_str(), NULL);
+	testPath = appPath+"/../Samples";
+	if(!resolved_path) resolved_path = realpath(testPath.c_str(), NULL);
+	testPath = appPath+"/../../Samples";
+	if(!resolved_path) resolved_path = realpath(testPath.c_str(), NULL);
+	testPath = appPath+"/../../../Samples";
+	if(!resolved_path) resolved_path = realpath(testPath.c_str(), NULL);
+	testPath = appPath+"/../../../../Samples";
+	if(!resolved_path) resolved_path = realpath(testPath.c_str(), NULL);
+	testPath = appPath+"/../../../../../Samples";
+	if(!resolved_path) resolved_path = realpath(testPath.c_str(), NULL);
+	testPath = appPath+"/../../../../../../Samples";
+	if(!resolved_path) resolved_path = realpath(testPath.c_str(), NULL);
+	testPath = appPath+"/../../../../../../../Samples";
+	if(!resolved_path) resolved_path = realpath(testPath.c_str(), NULL);
+	testPath = appPath+"/../../../../../../../../Samples";
+	testPath = appPath+"/assets";
+	if(!resolved_path) resolved_path = realpath(testPath.c_str(), NULL);
+	testPath = appPath+"/../assets";
+	if(!resolved_path) resolved_path = realpath(testPath.c_str(), NULL);
+	testPath = appPath+"/../../assets";
+	if(!resolved_path) resolved_path = realpath(testPath.c_str(), NULL);
+	testPath = appPath+"/../../../assets";
+	if(!resolved_path) resolved_path = realpath(testPath.c_str(), NULL);
+	testPath = appPath+"/../../../../assets";
+	if(!resolved_path) resolved_path = realpath(testPath.c_str(), NULL);
+	testPath = appPath+"/../../../../../assets";
+	if(!resolved_path) resolved_path = realpath(testPath.c_str(), NULL);
+	testPath = appPath+"/../../../../../../assets";
+	if(!resolved_path) resolved_path = realpath(testPath.c_str(), NULL);
+	testPath = appPath+"/../../../../../../../assets";
+	if(!resolved_path) resolved_path = realpath(testPath.c_str(), NULL);
+	testPath = appPath+"/../../../../../../../../assets";
+
+    if(!resolved_path)
+        return "";
+    Rml::String out(resolved_path);
+#endif
+	printf("out = %s\n", out.c_str());
+	return out+"/";
+}
