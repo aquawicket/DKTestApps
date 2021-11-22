25d507f25586fea5e295a1b74aab5e42671eebed
aquawicket <aquawicket@hotmail.com>
2021-11-22 09:16:44 -0800

    commit from git

--- DKApps/SDLRml/FindSamplesRoot.cpp
+++ DKApps/SDLRml/FindSamplesRoot.cpp
@@ -4,13 +4,25 @@
 	#include "Shlwapi.h"
 #else
     #include <sys/stat.h>
+    #include <unistd.h>
 #endif
     
 
 Rml::String Shell::FindSamplesRoot() 
 {
-	Rml::String path = "";
-	
+    Rml::String path = "/Users/aquamac/digitalknob/DKTestApps/DKApps/SDLRml";
+    struct stat buffer;
+    if(stat ("/../../Samples/", &buffer) != 0){
+        printf("%s DOES NOT EXIST\n", path.c_str());
+        path = "";
+    }
+    else{
+        printf("********** PATH EXISTS **********");
+    }
+    char *buf;
+    buf=(char *)malloc(100*sizeof(char));
+    getcwd(buf,100);
+    printf("\n %s \n",buf);
 #ifdef RMLUI_PLATFORM_WIN32
 	if(path.empty() && PathFileExistsA("../Samples"))
 		path = "../Samples";
@@ -25,23 +37,27 @@ Rml::String Shell::FindSamplesRoot()
 	GetFullPathName(path.c_str(), 256, resolved_path, &fileExt);
 	Rml::String out(resolved_path);
 #else
-    struct stat buffer;
-    if(path.empty() && (stat ("../Samples", &buffer) == 0) )
-        path = "../Samples";
-    if(path.empty() && (stat ("../../Samples", &buffer) == 0) )
-        path = "../../Samples";
-    if(path.empty() && (stat ("../../../Samples", &buffer) == 0) )
-        path = "../../../Samples";
-    if(path.empty() && (stat ("../../../../Samples", &buffer) == 0) )
-        path = "../../../../Samples";
-    if(path.empty() && (stat ("../../../../../Samples", &buffer) == 0) )
-        path = "../../../../../Samples";
-    if(path.empty() && (stat ("../../../../../../Samples", &buffer) == 0) )
-        path = "../../../../../../Samples";
-    if (path.empty() && (stat ("../assets", &buffer) == 0) )
-        path = "../assets";
-    if (path.empty() && (stat ("../../assets", &buffer) == 0) )
-        path = "../../assets";
+    
+    if(path.empty() && (stat ("/../Samples/", &buffer) == 0) )
+        path = "/../Samples/";
+    if(path.empty() && (stat ("/../../Samples/", &buffer) == 0) )
+        path = "/../../Samples/";
+    if(path.empty() && (stat ("/../../../Samples/", &buffer) == 0) )
+        path = "/../../../Samples/";
+    if(path.empty() && (stat ("../../../../Samples/", &buffer) == 0) )
+        path = "../../../../Samples/";
+    if(path.empty() && (stat ("../../../../../Samples/", &buffer) == 0) )
+        path = "../../../../../Samples/";
+    if(path.empty() && (stat ("../../../../../../Samples/", &buffer) == 0) )
+        path = "../../../../../../Samples/";
+    if(path.empty() && (stat ("../../../../../../../Samples/", &buffer) == 0) )
+        path = "../../../../../../../Samples/";
+    if(path.empty() && (stat ("../../../../../../../../Samples/", &buffer) == 0) )
+        path = "../../../../../../../../Samples/";
+    if (path.empty() && (stat ("/../assets", &buffer) == 0) )
+        path = "/../assets";
+    if (path.empty() && (stat ("/../../assets", &buffer) == 0) )
+        path = "/../../assets";
 	char *resolved_path = realpath(path.c_str(), NULL);
     if(!resolved_path)
         return "";
