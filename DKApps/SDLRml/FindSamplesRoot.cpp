a9b40ff45c50497fbbd9c1ea78c871b67cdf41c5
aquawicket <aquawicket@hotmail.com>
2021-11-23 01:40:32 -0800

    commit from git

--- DKApps/SDLRml/FindSamplesRoot.cpp
+++ DKApps/SDLRml/FindSamplesRoot.cpp
@@ -33,7 +33,7 @@ static_assert(false, "filesystem unavalable");
 #endif
 #endif
 
-#define BUFSIZE 4096
+//#define BUFSIZE 4096
 
 Rml::String Shell::FindSamplesRoot()
 {
@@ -51,7 +51,6 @@ Rml::String Shell::FindSamplesRoot()
 	GetModuleFileName(NULL, buffer, MAX_PATH);
 	appPath = Rml::String(buffer);
 #endif // RMLUI_PLATFORM_WIN32
-
 #ifdef RMLUI_PLATFORM_MACOSX
 	char buf[PATH_MAX + 1] = { 0 };
 	uint32_t bufsize = PATH_MAX;
@@ -59,23 +58,25 @@ Rml::String Shell::FindSamplesRoot()
 		puts(buf);
 	appPath = Rml::String(buf);
 #endif // RMLUI_PLATFORM_MACOSX
-
 #ifdef RMLUI_PLATFORM_LINUX
 	char buf[PATH_MAX + 1] = { 0 };
 	if (!realpath("/proc/self/exe", buf))
 		printf("ERROR: could not get appPath from /proc/self/exe \n");
 	appPath = Rml::String(buf);
 #endif // RMLUI_PLATFORM_LINUX
-	printf("appPath = %s\n", appPath.c_str());
 
+
+	printf("appPath = %s\n", appPath.c_str());
+	printf("current_path = %s\n", fs::current_path());
 	Rml::String basePath = appPath + "/";
 	basePath = Rml::StringUtilities::Replace(basePath, '\\', '/');
 
 	for (unsigned int i = 0; i < 15; i++) {
-		Rml::String tryPath = basePath + "Samples";
+		Rml::String tryPath = basePath + "Samples/";
 		printf("tryPath = %s\n", tryPath.c_str());
 
-#ifdef RMLUI_PLATFORM_WIN32
+
+#	ifdef RMLUI_PLATFORM_WIN32
 		std::string realPath = fs::absolute(tryPath).string();
 		realPath = Rml::StringUtilities::Replace(realPath, '\\', '/');
 		printf("realPath = %s\n", realPath.c_str());
@@ -89,8 +90,8 @@ Rml::String Shell::FindSamplesRoot()
 			basePath = basePath + "../";
 			continue;
 		}
-#endif // RMLUI_PLATFORM_WIN32
-#ifdef RMLUI_PLATFORM_MACOSX
+#	endif // RMLUI_PLATFORM_WIN32
+#	ifdef RMLUI_PLATFORM_MACOSX
 		/*
 		char* realPath = NULL;
 		struct stat info;
@@ -121,8 +122,8 @@ Rml::String Shell::FindSamplesRoot()
 			continue;
 		}
 
-#endif // RMLUI_PLATFORM_MACOSX
-#ifdef RMLUI_PLATFORM_LINUX
+#	endif // RMLUI_PLATFORM_MACOSX
+#	ifdef RMLUI_PLATFORM_LINUX
 		/*
 		char* realPath = NULL;
 		struct stat info;
@@ -152,8 +153,9 @@ Rml::String Shell::FindSamplesRoot()
 			basePath = basePath + "../";
 			continue;
 		}
-#endif // RMLUI_PLATFORM_LINUX
+#	endif // RMLUI_PLATFORM_LINUX
 	}
+
 	printf("ERROR: could not locate assets path \n");
 	return "";
 }
