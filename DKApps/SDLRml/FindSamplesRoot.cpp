07bf563e38c732c1e206779825c67d3adf080cdf
aquawicket <aquawicket@hotmail.com>
2021-11-22 23:53:27 -0800

    commit from git

--- DKApps/SDLRml/FindSamplesRoot.cpp
+++ DKApps/SDLRml/FindSamplesRoot.cpp
@@ -15,7 +15,9 @@
 	#include <unistd.h>         // readlink
 	#include <linux/limits.h>   // PATH_MAX
 #endif
-    
+#include <filesystem>
+
+#define BUFSIZE 4096
 
 Rml::String Shell::FindSamplesRoot()
 {
@@ -54,46 +56,88 @@ Rml::String Shell::FindSamplesRoot()
 	testPath = Rml::StringUtilities::Replace(testPath, '\\', '/');
 
 	for (unsigned int i = 0; i < 15; i++) {
-		Rml::String doPath = testPath + "Samples/";
-		printf("doPath = %s\n", doPath.c_str());
+		Rml::String tryPath = testPath + "Samples/";
+		printf("tryPath = %s\n", tryPath.c_str());
 
 #ifdef RMLUI_PLATFORM_WIN32
-		char resolved_path[256];
-		char* fileExt;
-		if (PathFileExistsA(doPath.c_str())) {
-			doPath = Rml::StringUtilities::Replace(doPath, "\\", "/");
-			GetFullPathName(doPath.c_str(), 256, resolved_path, &fileExt);
-			printf("resolved_path = %s\n", doPath.c_str());
-			return doPath + "/";
+		//if (PathFileExistsA(tryPath.c_str())) {
+		if(std::filesystem::exists(tryPath)){
+			printf("	PATH FOUND\n");
+			tryPath = Rml::StringUtilities::Replace(tryPath, "\\", "/");
+			//char realPath[256];
+			//TCHAR** lppPart = { NULL };
+			//if (GetFullPathName(tryPath.c_str(), BUFSIZE, realPath, lppPart)) {
+			std::error_code ec;
+			std::string realPath = std::filesystem::absolute(tryPath, ec).u8string();
+			if (ec) 
+				printf("ERROR: GetFullPathName(): %s\n", ec.message());
+			else{
+				realPath = Rml::StringUtilities::Replace(realPath, '\\', '/');
+				printf("realPath = %s\n", realPath.c_str());
+				return realPath + "/";
+			}
 		}
 		else {
+			printf("  not found\n");
 			testPath = testPath + "../";
 			continue;
 		}
 #endif
 #ifdef RMLUI_PLATFORM_MACOSX
-		char* resolved_path = NULL;
+		/*
+		char* realPath = NULL;
 		struct stat info;
-		resolved_path = realpath(doPath.c_str(), NULL);
-		if ((stat(resolved_path, &info) != 0) && (info.st_mode & S_IFDIR)) {
-			if (resolved_path) {
-				printf("resolved_path = %s\n", resolved_path);
-				return Rml::String(resolved_path) + "/";
+		realPath = realpath(tryPath.c_str(), NULL);
+		if ((stat(realPath, &info) != 0) && (info.st_mode & S_IFDIR)) {
+			printf("  PATH FOUND\n");
+			if (realPath) {
+				printf("realPath = %s\n", realPath);
+				return Rml::String(realPath) + "/";
 			}
 		}
-		testPath = testPath + "../";
+		else {
+			printf("  not found\n");
+			testPath = testPath + "../";
+		}
+		*/
+		//if (PathFileExistsA(tryPath.c_str())) {
+		if (std::filesystem::exists(tryPath)) {
+			printf("	PATH FOUND\n");
+			//tryPath = Rml::StringUtilities::Replace(tryPath, "\\", "/");
+			//char realPath[256];
+			//TCHAR** lppPart = { NULL };
+			//if (GetFullPathName(tryPath.c_str(), BUFSIZE, realPath, lppPart)) {
+			std::error_code ec;
+			std::string realPath = std::filesystem::absolute(tryPath, ec).u8string();
+			if (ec)
+				printf("ERROR: GetFullPathName(): %s\n", ec.message());
+			else {
+				realPath = Rml::StringUtilities::Replace(realPath, '\\', '/');
+				//printf("realPath = %s\n", realPath.c_str());
+				return realPath + "/";
+			}
+		}
+		else {
+			printf("  not found\n");
+			testPath = testPath + "../";
+			continue;
+		}
 #endif
 #ifdef RMLUI_PLATFORM_LINUX
-		char* resolved_path = NULL;
+		char* realPath = NULL;
 		struct stat info;
-		if (stat(doPath.c_str(), &info) != 0) {
-			resolved_path = realpath(doPath.c_str(), buf);
-			if (resolved_path) {
-				printf("resolved_path = %s\n", resolved_path);
-				return Rml::String(resolved_path);
+		if (stat(tryPath.c_str(), &info) != 0) {
+			printf("  PATH FOUND");
+			realPath = realpath(tryPath.c_str(), buf);
+			if (realPath) {
+				printf("realPath = %s\n", realPath);
+				return Rml::String(realPath);
 			}
 		}
-		testPath = testPath + "../";
+		else {
+			printf("  not found");
+			testPath = testPath + "../";
+		}
 #endif
 	}
 	printf("ERROR: could not locate assets path \n");
