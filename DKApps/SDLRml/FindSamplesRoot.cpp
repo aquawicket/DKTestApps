fe2774de350d1bf6c6e98fb6cdd1cd58e566a55c
aquawicket <aquawicket@hotmail.com>
2021-11-22 18:16:36 -0800

    commit from git

--- DKApps/SDLRml/FindSamplesRoot.cpp
+++ DKApps/SDLRml/FindSamplesRoot.cpp
@@ -1,5 +1,7 @@
 #include <RmlUi/Core.h>
+#include <RmlUi/Core/StringUtilities.h>
 #include "Shell.h"
+
 #ifdef RMLUI_PLATFORM_WIN32
 	#include "Shlwapi.h"
 #endif
@@ -21,52 +23,57 @@ Rml::String Shell::FindSamplesRoot()
     path = "";
 #else
     path = "";
-#endif
+#endif	
 
-
-#ifdef RMLUI_PLATFORM_WIN32
-	if(path.empty() && PathFileExistsA("../Samples"))
-		path = "../Samples";
-	if(path.empty() && PathFileExistsA("../../Samples"))
-		path = "../../Samples";
-	if(path.empty() && PathFileExistsA("../../../Samples"))
-		path = "../../../Samples";
-	if (path.empty() && PathFileExistsA("../assets"))
-		path = "../assets";
-	if (path.empty() && PathFileExistsA("../../assets"))
-		path = "../../assets";
-	if (path.empty() && PathFileExistsA("../../../assets"))
-		path = "../../../assets";
-	char *fileExt;
-	char resolved_path[256];
-	GetFullPathName(path.c_str(), 256, resolved_path, &fileExt);
-	return Rml::String(resolved_path);
-#endif
-
-#ifdef RMLUI_PLATFORM_UNIX
-	Rml::String testPath = "";
-	char* resolved_path = NULL;
-	char buf[PATH_MAX + 1] = {0};
 	Rml::String appPath = "";
 	
+	#ifdef RMLUI_PLATFORM_WIN32
+		char buffer[MAX_PATH];
+		GetModuleFileName( NULL, buffer, MAX_PATH );
+		appPath = Rml::String(buffer);
+	#endif
+	
 	#ifdef RMLUI_PLATFORM_MACOSX
-		uint32_t bufsize = PATH_MAX;
+		char buf[MAX_PATH + 1] = {0};
+		uint32_t bufsize = MAX_PATH; //PATH_MAX;
 		if(!_NSGetExecutablePath(buf, &bufsize))
 			puts(buf);
 		appPath = Rml::String(buf);
 	#endif
+	
 	#ifdef RMLUI_PLATFORM_LINUX
+		char buf[PATH_MAX + 1] = {0};
 		if(!realpath("/proc/self/exe", buf))
 			printf("ERROR: could not get appPath from /proc/self/exe \n");
 		appPath = Rml::String(buf);
 	#endif
 	printf("appPath = %s\n", appPath.c_str());
+
+	Rml::String testPath = appPath +"/";
+	testPath = Rml::StringUtilities::Replace(testPath, '\\', '/');
+	char resolved_path[256];
+	//char* resolved_path = NULL;
 	
-	testPath = appPath + "/";
-	for(unsigned int i=0; i<15; i++){
-		Rml::String doPath = testPath+"Samples";
+	for (unsigned int i = 0; i < 15; i++) {
+		Rml::String doPath = testPath + "Samples";
 		printf("doPath = %s\n", doPath.c_str());
-		resolved_path = realpath(testPath.c_str(), NULL);
+
+#ifdef RMLUI_PLATFORM_WIN32
+		char* fileExt;
+		if (PathFileExistsA(doPath.c_str())) {
+			doPath = Rml::StringUtilities::Replace(doPath, "\\", "/");
+			GetFullPathName(doPath.c_str(), 256, resolved_path, &fileExt);
+			printf("resolved_path = %s\n", doPath.c_str());
+			return doPath+"/";
+		}
+		else{
+			testPath = testPath + "../";
+			continue;
+		}
+		#else		
+			resolved_path = realpath(testPath.c_str(), NULL);
+		#endif
+		
 		if(resolved_path){
 			printf("resolved_path = %s\n", resolved_path);
 			return Rml::String(resolved_path);
@@ -74,20 +81,6 @@ Rml::String Shell::FindSamplesRoot()
 		testPath = testPath + "../";
 	}
 	
-	/*
-	memset(real_buf, 0, sizeof real_buf);
-	if(resolved_path = realpath(testPath.c_str(), real_buf)) { return Rml::String(resolved_path); }
-	testPath = appPath+"/../Samples";
-*/
-
-    if(!resolved_path){
-        printf("ERROR: could not locate assets path \n");
-		return "";
-	}
-    //Rml::String out(real_buf);
-#endif
-
-
-	//printf("out = %s\n", out.c_str());
-	//return out+"/";
+	printf("ERROR: could not locate assets path \n");
+	return "";
 }
