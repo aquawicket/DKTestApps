496d53a6d36f879bcd5094b6a738f8a7d62e5dcf
aquawicket <aquawicket@hotmail.com>
2021-12-05 02:35:51 -0800

    commit from git

---
+++ DKApps/VCMacros/main.cpp
@@ -0,0 +1,26 @@
+// This will allow you to print out the predefined macros in Microsoft Visual C++
+// To print C predefiend macros:    cl.exe /B1Macros.exe macros.c  
+// To print C++ predefiend macros:  cl.exe /BxMacros.exe macros.cpp
+// https://stackoverflow.com/a/3672331/688352
+
+// The Microsoft C/C++ compiler allows an alternative compiler front-end to be invoked using the /B1 and /Bx 
+// command line switches for .c and .cpp files respectively. The command-line interface module CL.exe passes 
+// a list of options to the replacement compiler front-end via the MSC_CMD_FLAGS environment variable. 
+// This list of options includes -D macro definitions for some of the predefined macros.
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    char* p;
+
+    if ((p = getenv("MSC_CMD_FLAGS")) != NULL)
+        printf("MSC_CMD_FLAGS:n%sn", p);
+
+    if ((p = getenv("MSC_IDE_FLAGS")) != NULL)
+        printf("MSC_IDE_FLAGS:n%sn", p);
+
+    return EXIT_FAILURE;
+}
