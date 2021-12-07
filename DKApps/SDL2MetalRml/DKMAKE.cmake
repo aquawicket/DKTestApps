f3905ec8db97ade4041edf4821036aa06d0bdb55
aquawicket <aquawicket@hotmail.com>
2021-12-07 00:27:22 -0800

    commit from git

--- DKApps/SDL2MetalRml/DKMAKE.cmake
+++ DKApps/SDL2MetalRml/DKMAKE.cmake
@@ -1,5 +1,3 @@
-DKDEPEND(opengl)
-#DKDEPEND(agl)
 DKDEPEND(xquartz)
 DKDEPEND(libx11-dev)
 DKDEPEND(shlwapi.lib)
@@ -14,30 +12,10 @@ DKDEPEND(sdl2)
 DKDEPEND(sdl2_image)
 DKDEPEND(rmlui)
 
-# import rmlui sdl2 sample source
-DKCOPY(${RMLUI}/Samples/basic/sdl2/src/ ${DKPROJECT}/ FALSE)
+# import rmlui shell sample source
 DKCOPY(${RMLUI}/Samples/shell/include/Shell.h ${DKPROJECT}/Shell.h FALSE)
 DKCOPY(${RMLUI}/Samples/shell/src/Shell.cpp ${DKPROJECT}/Shell.cpp FALSE)
 DKCOPY(${RMLUI}/Samples/shell/include/ShellFileInterface.h ${DKPROJECT}/ShellFileInterface.h FALSE)
 DKCOPY(${RMLUI}/Samples/shell/src/ShellFileInterface.cpp ${DKPROJECT}/ShellFileInterface.cpp FALSE)
 DKCOPY(${RMLUI}/Samples/shell/include/ShellRenderInterfaceExtensions.h ${DKPROJECT}/ShellRenderInterfaceExtensions.h FALSE)
 DKCOPY(${RMLUI}/Samples/shell/include/ShellSystemInterface.h ${DKPROJECT}/ShellSystemInterface.h FALSE)
-
-# rmlui shell
-#DKINCLUDE(${RMLUI}/Samples/shell/include)
-#ADD_SOURCE(${RMLUI}/Samples/shell/include/*.*)
-#ADD_SOURCE(${RMLUI}/Samples/shell/src/*.*)
-
-# rmlui unused shell when using sdl2
-#REMOVE_SOURCE(Input.*)
-#REMOVE_SOURCE(precompiled.*)
-#REMOVE_SOURCE(ShellOpenGL.*)
-#REMOVE_SOURCE(ShellRenderInterfaceOpenGL.*)
-#REMOVE_SOURCE(ShellRenderInterfaceExtensions.*)
-#REMOVE_SOURCE(ShellSystemInterface.*)
-
-# rmlui sdl2
-#DKINCLUDE(${RMLUI}/Samples/basic/sdl2/src)
-#ADD_SOURCE(${RMLUI}/Samples/basic/sdl2/src/RenderInterfaceSDL2.*)
-#ADD_SOURCE(${RMLUI}/Samples/basic/sdl2/src/SystemInterfaceSDL2.*)
-#ADD_SOURCE(${RMLUI}/Samples/basic/sdl2/src/main.cpp)
