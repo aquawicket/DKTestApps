e02b0416a18cfd90c744c1ec76750552c4d01fbe
aquawicket <aquawicket@hotmail.com>
2021-12-07 09:30:06 -0800

    commit from git

--- DKApps/sdl2vs-2.0.16/app/src/main/java/org/libsdl/app/SDLActivity.java
+++ DKApps/sdl2vs-2.0.16/app/src/main/java/org/libsdl/app/SDLActivity.java
@@ -165,7 +165,7 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
     protected String[] getLibraries() {
         return new String[] {
             "hidapi",
-            "SDL2",
+            // "SDL2",
             // "SDL2_image",
             // "SDL2_mixer",
             // "SDL2_net",
