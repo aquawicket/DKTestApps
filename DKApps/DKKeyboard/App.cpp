3c997165ef3819312e22bb0006e9a2ff0d91e97a
aquawicket <aquawicket@hotmail.com>
2021-12-13 06:50:42 -0800

    commit from git

--- DKApps/DKKeyboard/App.cpp
+++ DKApps/DKKeyboard/App.cpp
@@ -3,12 +3,21 @@
 #include "assets.h"
 #include "App.h"
 
-////////////////
-// For Doxygen purporses, this is a test.
-////////////////
-bool App::Init()
-{
-	DKINFO("App::Init()\n");
+#include <stdio.h>
+#include <stdlib.h>
+
+bool App::Init(){
+	
+	int i=0;
+	while(i<6){
+	printf("> \n");
+	int ch;
+    while ((ch=getchar()) != EOF)   /* read/print "abcde" from stdin */
+        printf("%d", ch);
+	
+	
+		i++;
+	}
 	return true;
 }
 
