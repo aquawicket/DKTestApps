a63aaa4ae4cb38d7dbb583c691d1c8ce69328463
aquawicket <aquawicket@hotmail.com>
2021-12-14 20:41:13 -0800

    commit from git

--- DKApps/SDL2Rml/FindSamplesRoot.cpp
+++ DKApps/SDL2Rml/FindSamplesRoot.cpp
@@ -1,4 +1,4 @@
-#include <RmlUi/Core/StringUtilities.h>  //Rml::StringUtilities::Replace
+#include <Shell.h>  //Rml::StringUtilities::Replace
 #include "RmlUiSDL2FileInterface.h"
 #include <iostream>
 
@@ -17,7 +17,7 @@
 #endif
 #include <sys/stat.h>
 
-Rml::String RmlUiSDL2FileInterface::FindSamplesRoot()
+Rml::String Shell::FindSamplesRoot()
 {
 	Rml::String path = "";
 	Rml::String appPath = "";
