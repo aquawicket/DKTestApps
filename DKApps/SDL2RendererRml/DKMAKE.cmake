9dd9282ff47f55255beda0385dea28cf6e963013
aquawicket <aquawicket@hotmail.com>
2021-12-20 01:24:08 -0800

    commit from git

--- DKApps/SDL2RendererRml/DKMAKE.cmake
+++ DKApps/SDL2RendererRml/DKMAKE.cmake
@@ -12,16 +12,4 @@ DKDEPEND(sdl2)
 DKDEPEND(sdl2_image)
 DKDEPEND(rmlui)
 DKDEPEND(mobile_core_services)
-DKDEPEND(DK)
-
 DKSET(USE_SDL2main ON)
-
-# import rmlui sdl2 sample source
-#DKCOPY(${RMLUI}/Samples/shell/include/Shell.h ${DKPROJECT}/Shell.h FALSE)
-#DKCOPY(${RMLUI}/Samples/shell/src/Shell.cpp ${DKPROJECT}/Shell.cpp FALSE)
-#DKCOPY(${RMLUI}/Samples/shell/include/ShellFileInterface.h ${DKPROJECT}/ShellFileInterface.h FALSE)
-#DKCOPY(${RMLUI}/Samples/shell/src/ShellFileInterface.cpp ${DKPROJECT}/ShellFileInterface.cpp FALSE)
-#DKCOPY(${RMLUI}/Samples/shell/include/ShellRenderInterfaceExtensions.h ${DKPROJECT}/ShellRenderInterfaceExtensions.h FALSE)
-#DKCOPY(${RMLUI}/Samples/shell/include/ShellSystemInterface.h ${DKPROJECT}/ShellSystemInterface.h FALSE)
-
-#DKCOPY(${RMLUI}/Samples/basic/sdl2_sdlrenderer/src/ ${DKPROJECT}/ FALSE)
