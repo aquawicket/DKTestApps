f4567f06ca939e2ba75c17fabf599535f155ec89
aquawicket <aquawicket@hotmail.com>
2021-12-21 07:33:35 -0800

    commit from git

--- DKApps/SDL2RendererRml/DKMAKE.cmake
+++ DKApps/SDL2RendererRml/DKMAKE.cmake
@@ -22,3 +22,7 @@ if(RELEASE)
 	dk_makeDirectory(${DKPROJECT}/${OS}/${RELEASE_DIR}/${APP_NAME}.app/assets)
 	DKCOPY(${DKPROJECT}/Samples/ ${DKPROJECT}/${OS}/${RELEASE_DIR}/${APP_NAME}.app/Samples TRUE)
 endif()
+add_custom_command(TARGET ${APP_NAME} PRE_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy_directory 
+              ${CMAKE_CURRENT_LIST_DIR}/Samples $<TARGET_FILE_DIR:ExampleTestTarget>
+)
