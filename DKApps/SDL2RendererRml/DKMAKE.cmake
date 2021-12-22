0d00f3d212d711c9f5920680c1369b1145cd1d84
aquawicket <aquawicket@hotmail.com>
2021-12-21 22:53:28 -0800

    commit from git

--- DKApps/SDL2RendererRml/DKMAKE.cmake
+++ DKApps/SDL2RendererRml/DKMAKE.cmake
@@ -1,14 +1,13 @@
-DKDEPEND(xquartz)               # mac, ios
-DKDEPEND(libx11-dev)            # linux
-DKDEPEND(shlwapi.lib)           # win
-DKDEPEND(zlib)                  # png
-DKDEPEND(giflib)                # sdl_image
-DKDEPEND(libpng)                # sdl_imgae
-DKDEPEND(libjpeg-turbo)         # sdl_image
-DKDEPEND(libwebp)               # sdl_image
-DKDEPEND(tiff)                  # sdl_image
-DKDEPEND(glew)                  # win, ma, linux
-DKDEPEND(sdl2)                  # 
-DKDEPEND(sdl2_image)            #  
-DKDEPEND(rmlui)                 # 
-#DKDEPEND(mobile_core_services) #iOS
+DKDEPEND(xquartz)
+DKDEPEND(libx11-dev)
+DKDEPEND(shlwapi.lib)
+DKDEPEND(zlib)
+DKDEPEND(giflib)
+DKDEPEND(libpng)
+DKDEPEND(libjpeg-turbo)
+DKDEPEND(libwebp)
+DKDEPEND(tiff)
+DKDEPEND(glew)
+DKDEPEND(sdl2)
+DKDEPEND(sdl2_image)
+DKDEPEND(rmlui)
