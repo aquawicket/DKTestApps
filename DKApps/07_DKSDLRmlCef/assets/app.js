55e646168b47da0520750c3e9fe6cdea1587880e
aquawicket <aquawicket@hotmail.com>
2022-04-27 02:17:52 -0700

    commit from git

--- DKApps/07_DKSDLRmlCef/assets/app.js
+++ DKApps/07_DKSDLRmlCef/assets/app.js
@@ -4,6 +4,6 @@ if(CPP_DK_GetBrowser() === "RML"){
 	CPP_DK_Create("DKRml");
 	CPP_DK_Create("DKSDLText");
 }
-location.href = "chrome://gpu";
+//location.href = "chrome://gpu";
 //location.href = CPP_DKAssets_LocalAssets()+"index.html";
-//location.href = "http://www.duckduckgo.com";
+location.href = "http://www.duckduckgo.com";
