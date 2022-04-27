8fed902c979cf5408f8c8c11117b841d8c1d1408
aquawicket <aquawicket@hotmail.com>
2022-04-27 03:48:23 -0700

    commit from git

--- DKApps/07_DKSDLRmlCef/assets/app.js
+++ DKApps/07_DKSDLRmlCef/assets/app.js
@@ -1,14 +1,11 @@
 var USE_CEF = 1
 
-//var url = "chrome://gpu";
+var url = "chrome://gpu";
 //const url = "http://duckduckgo.com"
 //const url = "http://www.google.com/"
 //const url = "http://127.0.0.1:2393"
-const url = CPP_DKAssets_LocalAssets()+"index.html"
+//const url = CPP_DKAssets_LocalAssets()+"index.html"
 //const url = CPP_DKAssets_LocalAssets()+"DKWebTest/index.html"
-
-var width = 800;//CPP_DKWindow_GetWidth()
-var height = 600;//CPP_DKWindow_GetHeight()
 
 
 ////// RmlUi
@@ -19,6 +16,9 @@ if(CPP_DK_GetBrowser() === "RML"){
 	//CPP_DK_Create("DKSDLText")
 }
 
+var width = CPP_DKWindow_GetWidth()
+var height = CPP_DKWindow_GetHeight()
+
 //location.href = url
 
 
@@ -27,7 +27,16 @@ if(CPP_DK_GetBrowser() === "RML"){
 CPP_DK_Create("DKCef,Cef,0,0,"+width+","+height+","+url)
 CPP_DKCef_NewBrowser("Cef",0,0,width,height,url)
 //CPP_DKCef_ShowDevTools(0)
+
 window.addEventListener("keydown", function mykeydown(event){
 	if(event.key === "F12")
 		CPP_DKCef_ShowDevTools(0)	
+})
+
+window.addEventListener("resize", function onresize(event){
+	//console.log("User_OnEvent(): resize\n")
+	//CPP_DKCef_SetSize("CefSDL", 100, 100)
+	var width = CPP_DKWindow_GetWidth()
+	var height = CPP_DKWindow_GetHeight()
+	DK_CallFunc("CefSDL::OnResize", "0,0,"+String(width)+","+String(height))
 })
