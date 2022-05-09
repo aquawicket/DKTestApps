f4110d0a82dcdf4346ae4f08eaeadaafdc4933d0
aquawicket <aquawicket@hotmail.com>
2022-05-09 13:49:56 -0700

    commit from git

--- DKApps/06_DKSDLCef/assets/app.js
+++ DKApps/06_DKSDLCef/assets/app.js
@@ -2,13 +2,11 @@ var USE_CEF = 1
 var url = "chrome://gpu";
 
 CPP_DK_Create("DKDuktapeDom")
-
 CPP_DK_Create("DKSDLWindow")
-var width = CPP_DKWindow_GetWidth()
-var height = CPP_DKWindow_GetHeight()
-
 CPP_DK_Create("DKSDLText")
 CPP_DK_Create("DKCef") //,"CefSDL",0,0,width,height,url)
+var width = CPP_DKWindow_GetWidth()
+var height = CPP_DKWindow_GetHeight()
 CPP_DKCef_NewBrowser("CefSDL", 0, 0, width, height, url)
 
 //var currentBrowser = CPP_DKCef_GetCurrentBrowser("CefSDL")
--- DKApps/07_DKSDLRmlCef/assets/app.js
+++ DKApps/07_DKSDLRmlCef/assets/app.js
@@ -1,25 +1,18 @@
 var USE_CEF = 1
-
 var url = "chrome://gpu";
-//const url = "http://duckduckgo.com"
-//const url = "http://www.google.com/"
-//const url = "http://127.0.0.1:2393"
-//const url = CPP_DKAssets_LocalAssets()+"index.html"
-//const url = CPP_DKAssets_LocalAssets()+"DKWebTest/index.html"
-
 
 ////// RmlUi
-if(CPP_DK_GetBrowser() === "RML"){
+//if(CPP_DK_GetBrowser() === "RML"){
 	CPP_DK_Create("DKDuktapeDom")
 	CPP_DK_Create("DKSDLWindow")    //DO NOT Create DKSDLWindow directly
 	CPP_DK_Create("DKSDLText")
 	CPP_DK_Create("DKRml")
-}
+//}
 
 var width = CPP_DKWindow_GetWidth()
 var height = CPP_DKWindow_GetHeight()
 
-location.href = CPP_DKAssets_LocalAssets()+"index.html"
+//location.href = CPP_DKAssets_LocalAssets()+"index.html"
 
 
 
@@ -28,6 +21,8 @@ CPP_DK_Create("DKCef") //,Cef,0,0,"+width+","+height+","+url)
 CPP_DKCef_NewBrowser("Cef",0,0,width,height,url)
 //CPP_DKCef_ShowDevTools(0)
 
+
+/*
 window.addEventListener("keydown", function mykeydown(event){
 	if(event.key === "F12")
 		CPP_DKCef_ShowDevTools(0)	
@@ -40,3 +35,4 @@ window.addEventListener("resize", function onresize(event){
 	var height = CPP_DKWindow_GetHeight()
 	DK_CallFunc("CefSDL::OnResize", "0,0,"+String(width)+","+String(height))
 })
+*/
