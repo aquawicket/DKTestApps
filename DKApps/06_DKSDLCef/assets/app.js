c3b0de2b3edb8dc023673a6ad0fe4d9e33fe261e
aquawicket <aquawicket@hotmail.com>
2022-05-09 01:05:15 -0700

    commit from git

--- DKApps/06_DKSDLCef/assets/app.js
+++ DKApps/06_DKSDLCef/assets/app.js
@@ -1,10 +1,12 @@
 var USE_CEF = 1
 var url = "chrome://gpu";
-var width = 800;//CPP_DKWindow_GetWidth()
-var height = 600;//CPP_DKWindow_GetHeight()
 
 CPP_DK_Create("DKDuktapeDom")
+
 CPP_DK_Create("DKSDLWindow")
+var width = CPP_DKWindow_GetWidth()
+var height = CPP_DKWindow_GetHeight()
+
 CPP_DK_Create("DKSDLText")
 CPP_DK_Create("DKCef")//,"CefSDL",0,0,width,height,url)
 CPP_DKCef_NewBrowser("CefSDL",0,0,width,height,url)
@@ -16,12 +18,11 @@ CPP_DKCef_NewBrowser("CefSDL",0,0,width,height,url)
 //CPP_DK_Create("DKDebug/DKDebug.js")
 
 //DKAddEvent("window", "resize", User_OnEvent)
-//window.addEventListener("resize", User_OnEvent)
+window.addEventListener("resize", User_OnEvent)
 
 
 
 function User_OnEvent(event){
-	/*
 	if(event.type == "resize"){
 		console.log("User_OnEvent(): resize\n")
 		//CPP_DKCef_SetSize("CefSDL", 100, 100)
@@ -29,5 +30,4 @@ function User_OnEvent(event){
 		var height = CPP_DKWindow_GetHeight()
 		DK_CallFunc("CefSDL::OnResize", "0,0,"+String(width)+","+String(height))
 	}
-	*/
 }
